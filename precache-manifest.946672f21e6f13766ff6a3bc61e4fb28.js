de98e8d35bfbb9ca0a4afd6efda8e3efc60725dc
theericfrost <theericfrost@mail.ru>
2020-02-10 13:07:03 +0300

    Updates

---
+++ precache-manifest.946672f21e6f13766ff6a3bc61e4fb28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0581d8239eea4aecb60d174134df78",
+    "url": "/holidays/index.html"
+  },
+  {
+    "revision": "fcf8e120f7d382320b6c",
+    "url": "/holidays/static/css/main.e7ceec3e.chunk.css"
+  },
+  {
+    "revision": "2c325e766d100d0a9a90",
+    "url": "/holidays/static/js/2.a8247847.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/holidays/static/js/2.a8247847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf8e120f7d382320b6c",
+    "url": "/holidays/static/js/main.03942d6f.chunk.js"
+  },
+  {
+    "revision": "3101f31bc45f5880bcad",
+    "url": "/holidays/static/js/runtime-main.1d024b95.js"
+  }
+]);
